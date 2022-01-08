0c54d0b89965b8e364861da83c27498eb3100e97
michailow <thererik@icloud.com>
2022-01-08 04:15:36 +0200

    Added tests

---
+++ test/test_docker.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def main():
+    urlGet = 'http://127.0.0.1:80'
+    r = requests.get(urlGet)
+    assert r.text == 'App started', 'Failture with API'
+    urlPost = 'http://127.0.0.1:80/postOutlet/tripadvisor_user.json'
+    r = requests.post(urlPost)
+    assert r.text == 'File tripadvisor_user.json posted',\
+        'Failture with DB'
+    print('Everything passed')
+
+
+if __name__ == '__main__':
+    main()
